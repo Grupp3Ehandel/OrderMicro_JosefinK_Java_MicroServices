ca43c6f873420f1daaf09c2cd2217a688e60d28c
josefinkl <josefin.klangefjall@gmail.com>
2025-03-25 14:03:36 +0100

    first commit

---
+++ src/test/java/com/example/OrderMicro/OrderMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
