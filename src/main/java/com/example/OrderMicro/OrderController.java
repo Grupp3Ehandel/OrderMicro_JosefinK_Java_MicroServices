bad9b958c4737203e98f57605b1997facdb3a169
josefinkl <josefin.klangefjall@gmail.com>
2025-03-26 10:28:38 +0100

    Trying to get it to work...

--- src/main/java/com/example/OrderMicro/OrderController.java
+++ src/main/java/com/example/OrderMicro/OrderController.java
@@ -1,38 +1,61 @@
 package com.example.OrderMicro;
 
 import org.apache.catalina.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
-    private final WebClient webclient;
+//    private final WebClient webclient;
 
-    private final OrderRepository orderRepository;
+    OrderService orderService;
+    OrderRepository orderRepository;
 
-    public OrderController(WebClient.Builder webclientBuilder, OrderRepository orderRepository) {
-        this.webclient = webclientBuilder.baseUrl("http://localhost:8080/orders").build();
+    public OrderController(OrderRepository orderRepository, OrderService orderService) {
         this.orderRepository = orderRepository;
+        this.orderService = orderService;
     }
 
-    @GetMapping("/{id}")
-    public Mono<OrderResponse> getOrderById(@PathVariable Long id) {
+//    public OrderController(WebClient.Builder webclientBuilder, OrderRepository orderRepository) {
+//        this.webclient = webclientBuilder.baseUrl("http://localhost:8080/orders").build();
+//        this.orderRepository = orderRepository;
+//    }
 
-        return orderRepository.findById(id).map(order ->
-            webclient.get()
-                .uri("/users/" + order.getUserId())
-                .retrieve().bodyToMono(User.class)
-                .map(user -> new OrderResponse(order, user)))
-            .orElse(Mono.empty());
-    }
+//    @GetMapping("/{id}")
+//    public Mono<OrderResponse> getOrderById(@PathVariable Long id) {
+//
+//        return orderRepository.findById(id).map(order ->
+//            webclient.get()
+//                .uri("/users/" + order.getUserId())
+//                .retrieve().bodyToMono(User.class)
+//                .map(user -> new OrderResponse(order, (com.example.OrderMicro.User) user)))
+//            .orElse(Mono.empty());
+//    }
 
     @PostMapping
-    public Order createOrder(@RequestBody Order order) {
-        return orderRepository.save(order);
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order o1 = orderService.addOrder(order);
+        return ResponseEntity.ok().body(o1);
     }
 
 
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        if (orders.isEmpty()) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            return ResponseEntity.ok(orders);
+        }
+    }
 }
+
+
---
+++ src/main/java/com/example/OrderMicro/OrderService.java
@@ -0,0 +1,21 @@
+package com.example.OrderMicro;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    OrderRepository orderRepository;
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order addOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+}
